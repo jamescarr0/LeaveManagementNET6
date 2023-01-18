74fab8f18f86f9d49c6fa2d77209488986cdab2b
James Carr <jamescarr0@hotmail.com>
2023-01-18 16:09:24 +0000

    Implement View Model and Model automapper

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number Of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage = "Please enter a valid number of days")]
+        public int DefaultDays { get; set; }
+
+    }
+}
